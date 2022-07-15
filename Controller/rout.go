d164c500e74e6e02ef69bafc637c3fcd9c734a75
yuha-yuha <issiy0126@gmail.com>
2022-07-15 16:59:45 +0900

    Data関係のルーティング実装

--- Controller/rout.go
+++ Controller/rout.go
@@ -4,5 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.GET("Data/star/", StarData())
+	r.GET("Data/UserItem/", UserData())
+	r.GET("Data/Quize/", QuizeData())
+
 	return r
 }

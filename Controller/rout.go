f96cd35c12fc24627d34c060a18626fb44c1102c
yuha-yuha <issiy0126@gmail.com>
2022-08-18 16:31:07 +0900

    UPDATE 各APIのルーティング実装をした

--- Controller/rout.go
+++ Controller/rout.go
@@ -5,9 +5,12 @@ import "github.com/gin-gonic/gin"
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("Data/star/", StarData())
-	r.GET("Data/UserItem/", UserData())
-	r.GET("Data/Quize/", QuizeData())
+	r.GET("/OrigStar/Get/", StarData())       //　オリジナル星座のデータをレスポンスするAPI
+	r.GET("/UserItem/Get/", GetUserItem())    // ユーザーのアイテムデータをレスポンスするAPI
+	r.POST("/UserItem/Post/", PostUserItem()) // ユーザーのアイテムデータ更新内容を受け取るAPI
+	r.GET("/Quize/Get/", QuizeGet())          // クイズデータをランダムに返すAPI
+	r.POST("Auth/", UserAuth())               // ユーザー認証API
+	r.GET("/Pub/", Pub())                     // サーバー側からのイベントをレスポンスするAPI
 
 	return r
 }

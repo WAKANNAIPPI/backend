f90ec9d3606fc230ef670df7791c995e33abb275
yuha-yuha <issiy0126@gmail.com>
2022-08-23 17:21:10 +0900

    UPDATE 各テーブルを作成した

--- setting.sql
+++ setting.sql
@@ -1,3 +1,41 @@
-create database wakannaippi
+create database wakannaippi;
+use wakannaippi;
+
+create table users(
+    user_id varchar(60) not null,
+    user_name varchar(60) not null,
+    user_password varchar(60) not null,
+    is_deleted boolean default 0,
+    primary key(user_id) 
+);
+
+create table original_constellations(
+    constellation_id varchar(60) not null,
+    constellation_name varchar(60),
+    user_id varchar(60) not null,
+    constellation_data int,
+    primary key(constellation_id),
+    foreign key(user_id)
+    references users(user_id)
+);
+
+create table user_items(
+    user_item_id varchar(60) not null,
+    user_id varchar(60) not null,
+    user_item_quantity int not null default 0,
+    primary key(user_item_id, user_id),
+    foreign key(user_id)
+    references users(user_id)
+);
+
+create table item_informations(
+    item_id varchar(60) not null,
+    item_name varchar(60),
+    primary key(item_id),
+    foreign key(item_id)
+    references user_items(user_item_id)
+);
+
+
 
 

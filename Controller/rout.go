d59c9a5a156ff1053b67665eddeebaec03b5b756
yuha-yuha <issiy0126@gmail.com>
2022-09-02 23:12:08 +0900

    update ユーザ認証、APIの認証ガードのルーティング実装

--- Controller/rout.go
+++ Controller/rout.go
@@ -1,16 +1,30 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/OrigStar/Get/", StarData())       //　オリジナル星座のデータをレスポンスするAPI
-	r.GET("/UserItem/Get/", GetUserItem())    // ユーザーのアイテムデータをレスポンスするAPI
-	r.POST("/UserItem/Post/", PostUserItem()) // ユーザーのアイテムデータ更新内容を受け取るAPI
-	r.GET("/Quize/Get/", QuizeGet())          // クイズデータをランダムに返すAPI
-	r.POST("Auth/", UserAuth())               // ユーザー認証API
-	r.GET("/Pub/", Pub())                     // サーバー側からのイベントをレスポンスするAPI
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.POST("/login/post", model.Userlogin())
+
+	AuthUserGroup := r.Group("/auth")
+	AuthUserGroup.Use(middleware.LoginCheck())
+	{
+
+		r.GET("/OrigStar/Get/", model.GetStarData())    //　オリジナル星座のデータをレスポンスするAPI
+		r.GET("/UserItem/Get/", model.GetUserItem())    // ユーザーのアイテムデータをレスポンスするAPI
+		r.POST("/UserItem/Post/", model.PostUserItem()) // ユーザーのアイテムデータ更新内容を受け取るAPI
+		r.GET("/Quize/Get/", model.QuizeGet())          // クイズデータをランダムに返すAPI
+		r.GET("/Pub/", model.Pub())                     // サーバー側からのイベントをレスポンスするAPI
+		r.POST("/OrigStar/Post/", model.PostStarData())
+	}
 
 	return r
 }

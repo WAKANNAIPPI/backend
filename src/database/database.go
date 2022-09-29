32214e366c938286257dd6ca3f44d978fa3af43b
yuha <issiy0126@gmail.com>
2022-09-29 10:30:39 +0900

    update gitgnore 適応

---
+++ src/database/database.go
@@ -0,0 +1,165 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/ini.v1"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+//ユーザテーブル
+type User struct {
+	Id        string `gorm:"column:user_id" json:"userId"`
+	Password  string `gorm:"column:user_password" json:"userPass"`
+	Name      string `gorm:"column:user_name" json:"userName"`
+	IsDeleted bool   `gorm:"column:is_deleted" json:"-"` //論理削除フラグ
+}
+
+//ユーザitem情報テーブル
+type User_item struct {
+	Iid      string `gorm:"column:user_item_id" json:"itemId"`        //アイテムid
+	Uid      string `gorm:"column:user_id" json:"-"`                  //ユーザid
+	Quantity int    `gorm:"column:user_item_quantity" json:"itemQty"` //アイテム数量
+}
+
+type User_constellations struct {
+	Cid  string `gorm:"column:user_constellation_id" json:"consteId"`     //星座ID
+	Name string `gorm:"column:user_constellation_name" json:"consteName"` //星座の名前
+	Uid  string `gorm:"column:user_id" json:"-"`                          //ユーザーid
+	Data int    `gorm:"column:user_constellation_data" json:"consteData"` //星座データ
+}
+
+//Item差分管理用(ユーザアイテムのjsonのやり取りに使う)
+type UserItemJson struct {
+	Iid  string `json:"itemId"`
+	Diff int    `json:"itemDiff"`
+}
+
+type UserConstellationJson struct {
+	Cid  string `json:"consteId"`
+	Name string `json:"consteName"`
+	Data int    `json:"consteData"`
+}
+
+type QuizeDataJson struct {
+	No       int    `json:"quizeNumber"`
+	Question string `json:"question"`
+	Choice1  string `json:"choice1"`
+	Choice2  string `json:"choice2"`
+	Choice3  string `json:"choice3"`
+	Ans      int    `json:"ans"`
+}
+
+func DBconnect() *gorm.DB {
+	//iniファイルをロード
+	dbCfg, err := ini.Load("database/db.ini")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	//GormのアクセスURLを代入
+	GormInfo := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbCfg.Section("db").Key("dbUserName").String(),
+		dbCfg.Section("db").Key("dbUserPass").String(),
+		dbCfg.Section("db").Key("dbHost").String(),
+		dbCfg.Section("db").Key("dbPort").String(),
+		dbCfg.Section("db").Key("dbName").String(),
+	)
+
+	//GORMによるDBアクセス
+	db, err := gorm.Open(mysql.Open(GormInfo), &gorm.Config{})
+
+	if err != nil {
+		log.Panic(err)
+	}
+	//アクセスされたオブジェクトを返す
+	return db
+}
+
+func CreateUser(u User) { //ユーザー作成関数
+	db := DBconnect()
+
+	//passwordのhash化
+	uPas := []byte(u.Password)
+	uHashedPas, _ := bcrypt.GenerateFromPassword(uPas, 5)
+	u.Password = string(uHashedPas)
+
+	//データベースに登録
+	db.Create(&u)
+
+}
+
+func GetUserData(u User) User { //ユーザ情報を取得する関数
+	db := DBconnect()
+	//UserIDを入れてレコードを特定
+	user := User{}
+	//単一レコードを引っ張ってくる
+	db.Where("user_id = ?", u.Id).First(&user)
+
+	log.Println("databaseReqUser:", u)
+	log.Println("databaseUser:", user)
+
+	return user
+}
+
+func SetUserItemData(u User, itemDiff []UserItemJson) { //クライアントにはアイテム名と更新されたアイテム数をjsonとして渡される前提
+	db := DBconnect()
+	// json形式 [{"ItemId":string, Quantity:int}]
+	for i := 0; i < len(itemDiff); i++ {
+		UserItemBefore := User_item{}
+		UserItemBefore.Iid = itemDiff[i].Iid
+		UserItemBefore.Uid = u.Id
+
+		UserItemAfter := UserItemBefore
+
+		log.Println("1:", UserItemAfter)
+
+		db.Where("user_item_id = ?", itemDiff[i].Iid).Where("user_id = ?", u.Id).First(&UserItemAfter)
+		log.Println("2", UserItemAfter)
+
+		UserItemAfter.Quantity += itemDiff[i].Diff
+		log.Println("itemQuantity = ", UserItemAfter)
+
+		db.Model(&UserItemBefore).Where("user_id = ?", u.Id).Where("user_item_id = ?", itemDiff[i].Iid).Update("user_item_quantity", UserItemAfter.Quantity)
+	}
+
+}
+
+func GetUserItemData(u User) []User_item {
+	db := DBconnect()
+
+	UserItem := []User_item{}
+
+	db.Find(&UserItem, "user_id=?", u.Id)
+
+	return UserItem
+}
+
+func CreateUserConstellationData(u User, uc UserConstellationJson) error {
+	db := DBconnect()
+
+	UserConstellation := User_constellations{}
+
+	UserConstellation.Uid = u.Id
+	UserConstellation.Name = uc.Name
+	UserConstellation.Cid = uc.Cid
+	UserConstellation.Data = uc.Data
+
+	err := db.Debug().Create(&UserConstellation).Error
+
+	return err
+}
+
+func GetUserConstellationData(u User) []User_constellations {
+	db := DBconnect()
+
+	uc := []User_constellations{}
+
+	db.Find(&uc, "user_id=?", u.Id) //あるユーザーの作った星座情報を一括取得
+
+	return uc
+}
---
+++ src/middleware/logincheck.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"src/database"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/koron/go-dproxy"
+)
+
+func LoginCheck(ctx *gin.Context) {
+	session := sessions.Default(ctx)
+
+	loginUserJson, err := dproxy.New(session.Get("loginUser")).String()
+
+	if err != nil {
+		ctx.Status(http.StatusUnauthorized)
+		log.Println(err)
+		log.Println("dproxy")
+		log.Println(session.Get("loginUser"))
+		ctx.Abort()
+	} else {
+		var UserInfo database.User
+
+		err := json.Unmarshal([]byte(loginUserJson), &UserInfo)
+
+		if err != nil {
+			ctx.Status(http.StatusUnauthorized)
+			log.Println("unmarshal")
+			ctx.Abort()
+		} else {
+			ctx.Next()
+		}
+	}
+}

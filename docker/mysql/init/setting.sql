c1c5e9fef637a05fbf102b496fa95a2b296daac2
yuha <issiy0126@gmail.com>
2022-10-10 22:25:00 +0900

    update default値変更、外部キー修正

--- docker/mysql/init/setting.sql
+++ docker/mysql/init/setting.sql
@@ -20,7 +20,7 @@ create table user_constellations(
 create table user_items(
     user_item_id varchar(60) not null,
     user_id varchar(60) not null,
-    user_item_quantity int not null default 0,
+    user_item_quantity int not null default 10,
     primary key(user_item_id, user_id),
     foreign key(user_id)
     references users(user_id)
@@ -31,7 +31,7 @@ create table conste_stars(
     user_constellation_id varchar(60) not null,
     conste_stored_star json,
     foreign key(user_constellation_id)
-    references user_constellation_id(user_constellation_id),
+    references user_constellations(user_constellation_id),
     primary key(id)
 );
 create table conste_lines(
@@ -39,7 +39,7 @@ create table conste_lines(
     user_constellation_id varchar(60) not null,
     conste_stored_line json,
     foreign key(user_constellation_id)
-    references user_constellation_id(user_constellation_id),
+    references user_constellations(user_constellation_id),
     primary key(id)
 );
 
